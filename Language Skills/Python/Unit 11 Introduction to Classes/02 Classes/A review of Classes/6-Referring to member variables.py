d3ca46bbc1cfcb6db1752506c6557c6f16fc6401
Roy van Dijk <r.e.l.v.dijk@student.tue.nl>
2016-11-18 17:06:01 +0100

    Update and rename 6-Referring to member variables .py to 6-Referring to member variables.py

--- Language Skills/Python/Unit 11 Introduction to Classes/02 Classes/A review of Classes/6-Referring to member variables.py
+++ Language Skills/Python/Unit 11 Introduction to Classes/02 Classes/A review of Classes/6-Referring to member variables.py
@@ -1,11 +1,11 @@
- class Car(object):
+class Car(object):
     condition = "new"
     def __init__(self, model, color, mpg):
         self.model = model
         self.color = color
         self.mpg = mpg
 
-my_car = Car("Chevv","GOLDEN", 1933)
+my_car = Car("DeLorean","silver", 88)
 
 print my_car.model
 print my_car.color
